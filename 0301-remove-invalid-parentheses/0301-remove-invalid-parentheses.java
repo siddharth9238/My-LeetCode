26239ac125e2ced7d7c081c0718fd7ca5ba477c8
Siddharth singh <singhsiddharth9238@gmail.com>
2026-07-15 17:23:00 +0530

    Time: 1 ms (99.82%), Space: 43.8 MB (92.32%) - LeetHub

---
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> ans = new ArrayList<>();
+        remove(s, ans, 0, 0, new char[]{'(', ')'});
+        return ans;
+    }
+
+    private void remove(String s, List<String> ans, int last_i, int last_j, char[] par) {
+        for (int count = 0, i = last_i; i < s.length(); i++) {
+            if (s.charAt(i) == par[0]) {
+                count++;
+            }
+            if (s.charAt(i) == par[1]) {
+                count--;
+            }
+            if (count >= 0) {
+                continue;
+            }
+            
+            for (int j = last_j; j <= i; j++) {
+                if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1])) {
+                    remove(s.substring(0, j) + s.substring(j + 1), ans, i, j, par);
+                }
+            }
+            return;
+        }
+        
+        String reversed = new StringBuilder(s).reverse().toString();
+        if (par[0] == '(') {
+            remove(reversed, ans, 0, 0, new char[]{')', '('});
+        } else {
+            ans.add(reversed);
+        }
+    }
+}
